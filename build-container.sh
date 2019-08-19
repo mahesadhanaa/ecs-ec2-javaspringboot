ab594915ce8c16f1f11e69c834081abf2c859f55
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-19 17:57:19 +0700

    change env build

--- build-container.sh
+++ build-container.sh
@@ -1,36 +1,31 @@
 #!/bin/sh
 
 # AWS Region that the ECS Cluster is in
-ECS_REGION='us-west-2'
+ECS_REGION='us-east-2'
 # Name of the ECS Cluster
 ECS_CLUSTER_NAME='name-of-ecs-cluster'
 # Name of the service on the ECS Cluster
-ECS_SERVICE_NAME='NameOfService'
+ECS_SERVICE_NAME='auth-service'
 # Name of the task definition used by the service
-ECS_TASK_DEFINITION_NAME='NameOfTaskDefinition'
+ECS_TASK_DEFINITION_NAME='auth-task'
 # Name of the ECR
-ECR_NAME='name-of-ecr'
+ECR_NAME='auth'
 # URI of the ECR
-ECR_URI='account-number.dkr.ecr.us-west-2.amazonaws.com'
+ECR_URI='095328455918.dkr.ecr.us-east-2.amazonaws.com'
 # Current timestamp, to use as a version tag.
 VERSION=$(date +%s)
 # Minimum targe AWS CLI version
 AWSCLI_VER_TAR=1.11.91
 
 # Build the Docker image from the Docker file.
-docker build --pull -t "${ECR_NAME}:latest" -f ./docker/Dockerfile .
-
+docker build --pull -t "${ECR_NAME}:latest" -f Dockerfile .
 # Tag the new Docker image to the remote repo, using the VERSION identifier
-docker tag "${ECR_NAME}:latest" "${ECR_URI}/${ECR_NAME}:${ENVIRONMENT}-${VERSION}"
-
+    # docker tag "${ECR_NAME}:latest" "${ECR_URI}/${ECR_NAME}:${ENVIRONMENT}-${VERSION}"
 # Tag the new Docker image to the remote repo, using the "latest-ENVIRONMENT" identifier 
-docker tag "${ECR_NAME}:latest" "${ECR_URI}/${ECR_NAME}:latest-${ENVIRONMENT}"
-
+docker tag "${ECR_NAME}:latest" "${ECR_URI}/${ECR_NAME}:latest-${VERSION}"
 # Login to AWS ECR
 $(aws ecr get-login --region "${ECS_REGION}")
-
 # Push to the remote ECR repo (VERSION identifier)
-docker push "${ECR_URI}/${ECR_NAME}:${ENVIRONMENT}-${VERSION}"
-
+    # docker push "${ECR_URI}/${ECR_NAME}:${ENVIRONMENT}-${VERSION}"
 # Push to the remote ECR repo (latest-ENVIRONMENT identifier)
-docker push "${ECR_URI}/${ECR_NAME}:latest-${ENVIRONMENT}"
+docker push "${ECR_URI}/${ECR_NAME}:latest-${VERSION}"
