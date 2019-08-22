deeb2bf497adf28cabc6e3794dc825294e4be80e
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-22 17:54:13 +0700

    update aws cli

--- awscli/build-container.sh
+++ awscli/build-container.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # build jar
-mvn clean package
+# mvn clean package
 
 # AWS Region that the ECS Cluster is in
 ECS_REGION='us-east-2'
@@ -14,11 +14,13 @@ ECS_TASK_DEFINITION_NAME='auth-task'
 # Name of the ECR
 ECR_NAME='auth'
 # URI of the ECR
-ECR_URI=''
+ECR_URI='095328455918.dkr.ecr.us-east-2.amazonaws.com'
 # Current timestamp, to use as a version tag.
 VERSION=$(date +%s)
 # Minimum targe AWS CLI version
 AWSCLI_VER_TAR=1.11.91
+# Check repository
+CHECK_REPOSITORY=`aws ecr create-repository --repository-name auth`  > /dev/null 2>&1
 
 # Build the Docker image from the Docker file.
 docker build --pull -t "${ECR_NAME}:latest" -f Dockerfile .
@@ -26,5 +28,9 @@ docker build --pull -t "${ECR_NAME}:latest" -f Dockerfile .
 docker tag "${ECR_NAME}:latest" "${ECR_URI}/${ECR_NAME}:latest-${VERSION}"
 # Login to AWS ECR
 $(aws ecr get-login --region "${ECS_REGION}")
-# Push to the remote ECR repo (latest-ENVIRONMENT identifier)
-docker push "${ECR_URI}/${ECR_NAME}:latest-${VERSION}"
+
+if  $CHECK_REPOSITORY | grep -q "An error occurred (RepositoryAlreadyExistsException) when calling the CreateRepository operation: The repository with name 'auth' already exists in the registry with id '095328455918'" ; then
+
+	echo "already exist" | xargs docker push "${ECR_URI}/${ECR_NAME}:latest-${VERSION}"
+
+fi
